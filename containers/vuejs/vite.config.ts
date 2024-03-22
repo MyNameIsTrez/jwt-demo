488d67fe6c82ebe7fa188c9fc3e88738fe60d5bf
MyNameIsTrez <welfje@gmail.com>
2024-03-22 16:01:27 +0100

    Add vuejs container

---
+++ containers/vuejs/vite.config.ts
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import VueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 2424,
+    host: '0.0.0.0' // This could be substituted with the host's IP
+  },
+  plugins: [
+    vue(),
+    VueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
