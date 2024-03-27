2b39f40b1dcff14d011a2d91f8eddd3ed6a9c093
MyNameIsTrez <welfje@gmail.com>
2024-03-27 14:16:16 +0100

    Increase expiration time to 30s

--- containers/nestjs/src/auth/auth.module.ts
+++ containers/nestjs/src/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { AuthGuard } from './auth.guard';
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '20s' },
+      signOptions: { expiresIn: '30s' },
     }),
   ],
   providers: [
