875586986ea5d25dd7658b309b4cecf9b62c04d8
MyNameIsTrez <welfje@gmail.com>
2024-03-27 14:08:53 +0100

    Remove comment

--- containers/nestjs/src/auth/auth.guard.ts
+++ containers/nestjs/src/auth/auth.guard.ts
@@ -31,11 +31,8 @@ export class AuthGuard implements CanActivate {
       throw new UnauthorizedException();
     }
     try {
-      //   const payload = await this.jwtService.verifyAsync(token, {
-      //     secret: jwtConstants.secret,
-      //   });
       const payload = await this.jwtService.verifyAsync(token);
-      // 💡 We're assigning the payload to the request object here
+      // We're assigning the payload to the request object here
       // so that we can access it in our route handlers
       request['user'] = payload;
     } catch {
